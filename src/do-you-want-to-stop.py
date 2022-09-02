3763844f83b5eff37da2e3b7275cbb7a8f7e3943
Ch-Bun <charlotte.bunnenberg@gmx.de>
2022-09-02 11:19:13 +0200

    solved exercise 3,4 and 5

--- src/do-you-want-to-stop.py
+++ src/do-you-want-to-stop.py
@@ -1,3 +1,12 @@
 
 # You should write a loop around this question, and keep asking until
 # the answer is "yes" (lower case).
+
+#While-loop
+while True:
+
+    # this asks the user if he/she wants to stop
+    answer = str(input("Do you want to stop?"))
+
+    if answer == 'yes':
+        break
--- src/print-1-10-growing.py
+++ src/print-1-10-growing.py
@@ -1,2 +1,7 @@
 
 # Print the numbers described in the exercise
+
+case_numbers = []
+for i in range(10):
+    case_numbers.append(int(i+1))
+    print(*case_numbers ,sep=" ", end="\n")
