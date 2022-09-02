91df67a2b2b23579c9c03ccf7966fa786a91b562
Ch-Bun <charlotte.bunnenberg@gmx.de>
2022-09-02 11:38:07 +0200

    solved pattern.py

--- src/pattern.py
+++ src/pattern.py
@@ -1,2 +1,9 @@
 
 # Print the pattern
+stars_list = []
+for i in range(5):
+    stars_list.append("*")
+    print(*stars_list ,sep=" ", end="\n")
+for j in range(4):
+    stars_list.pop(-1)
+    print(*stars_list ,sep=" ", end="\n")
