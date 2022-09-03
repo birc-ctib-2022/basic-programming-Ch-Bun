7780eef3ed38bf26f166a18703e764566f0960d8
Ch-Bun <charlotte.bunnenberg@gmx.de>
2022-09-03 12:21:32 +0200

    fixed password.py

--- src/password.py
+++ src/password.py
@@ -8,26 +8,27 @@ if len(sys.argv) != 2:
 password = sys.argv[1]
 is_valid = False
 
-# Do all the requirement checks here.
-
+#initialisation
 is_lower = False
 is_upper = False
 is_numeric = False
 is_special = False
 pw_length = 0
+
+# Do all the requirement checks here.
 for c in password:
-    pw_length =+1
+ 
+    pw_length += 1
     if c.islower():
         is_lower = True
     elif c.isupper():
         is_upper = True
     elif c.isnumeric():
         is_numeric = True
-    elif c in "$#@":
+    elif c in '$#@':
         is_special = True
-    
+#Check if all requirements are fullfilled   
 if is_lower and is_numeric and is_special and is_upper and 6 <= pw_length <= 16:
     is_valid = True
-
 
 print(is_valid)
